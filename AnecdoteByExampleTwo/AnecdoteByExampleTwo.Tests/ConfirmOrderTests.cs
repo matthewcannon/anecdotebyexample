9dfef240cebdd1f53aca4a0d51f7f08efe1ca965
matthewcannon <m.cannon.dev@gmail.com>
2012-12-07 17:28:48 +0000

    passes the first scenario

--- AnecdoteByExampleTwo/AnecdoteByExampleTwo.Tests/ConfirmOrderTests.cs
+++ AnecdoteByExampleTwo/AnecdoteByExampleTwo.Tests/ConfirmOrderTests.cs
@@ -5,20 +5,23 @@ using NUnit.Framework;
 namespace AnecdoteByExampleTwo.Tests
 {
     [TestFixture]
-    public class one_order_confirmation_email_is_sent
+    public class always : IHandle<IEvent>
     {
         IList<IEvent> _events;
+        EventAggregator _eventAggregator;
 
         [SetUp]
         public void SetUp()
         {
-            _events = new List<IEvent>();    
+            _events = new List<IEvent>();
+            _eventAggregator = new EventAggregator();
+            _eventAggregator.Register(this);
         }
 
         [Test]
-        public void test()
+        public void an_order_confirmation_email_is_sent()
         {
-            new ConfirmOrder().Execute();
+            new ConfirmOrder(_eventAggregator).Execute();
             OneOrderConfirmationEmailIsSent();
         }
 
@@ -26,20 +29,61 @@ namespace AnecdoteByExampleTwo.Tests
         {
             Assert.That(_events.Where(e => e is EmailSent<OrderConfirmationEmail>).Count(), Is.EqualTo(1));
         }
-    }
 
-    public class ConfirmOrder
-    {
-        public void Execute()
+        public void Handle(IEvent @event)
         {
+            _events.Add(@event);
         }
     }
 
-    internal class OrderConfirmationEmail : Email {}
+    internal class ConfirmOrder
+    {
+        readonly EventAggregator _eventAggregator;
 
-    internal class EmailSent<T> : IEvent where T : Email {}
+        public ConfirmOrder(EventAggregator eventAggregator)
+        {
+            _eventAggregator = eventAggregator;
+        }
 
-    internal class Email {}
+        public void Execute()
+        {
+            _eventAggregator.Publish(new EmailSent<OrderConfirmationEmail>());
+        }
+    }
 
-    internal interface IEvent {}
+    internal class EventAggregator
+    {
+        readonly IList<IHandle<IEvent>> _eventHandlers;
+
+        public EventAggregator()
+        {
+            _eventHandlers = new List<IHandle<IEvent>>();    
+        }
+
+        public void Publish(IEvent @event)
+        {
+            foreach(var eventHandler in _eventHandlers)
+            {
+                eventHandler.Handle(@event);
+            }
+        }
+
+        public void Register<T>(T eventHandler) where T : IHandle<IEvent>
+        {
+            _eventHandlers.Add(eventHandler);
+        }
+    }
+
+    internal interface IHandle<T> where T : IEvent
+    {
+        void Handle(T @event);
+    }
+
+    public class OrderConfirmationEmail : Email {}
+
+    public class EmailSent<T> : IEvent where T : Email {}
+
+    public class Email {}
+
+    public interface IEvent {}
 }
